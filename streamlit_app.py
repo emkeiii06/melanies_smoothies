11fb35d6b329d25abfd42e75708af9f7c91b956d
emkeiii06 <king.tumamao6@gmail.com>
2024-09-02 12:57:04 +0800

    Update streamlit_app.py
    added search_on in the for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ my_dataframe = session.table ("smoothies.public.fruit_options").select(col("FRUI
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 my_dataframe = session.table ("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
 
@@ -39,7 +39,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for x in ingredients_list:
-        ingredients_string += x + ' '
+        ingredients_string += x + ' ' 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader( x + ' Nutrition Information') 
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + x)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
